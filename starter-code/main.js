e3944635f5e9f9ea0109c746660e1ae23fcacfeb
Matt Kersner <mattkersner@Matts-Air.home>
2016-11-18 14:58:57 -0500

    furthered game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,15 @@
 console.log("JS file is connected to HTML! Woo!")
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+if (cardThree === cardFour) {
+	alert("You found a match!")
+}
+else if (cardOne === cardTwo) {
+	alert("You found a match!")
+}
+else {
+	alert("Sorry, try again.")
+}
