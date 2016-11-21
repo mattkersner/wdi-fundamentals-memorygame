47c79804c831e627b2676007b00a78f4577eb816
Matt Kersner <mattkersner@gmail.com>
2016-11-21 11:22:56 -0500

    test

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,11 +5,9 @@ var cardThree = "king";
 var cardFour = "king";
 
 if (cardThree === cardFour) {
-	alert("You found a match!")
-}
-else if (cardOne === cardTwo) {
-	alert("You found a match!")
-}
-else {
-	alert("Sorry, try again.")
+	alert("You found a match!");
+} else if (cardOne === cardTwo) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
 }
