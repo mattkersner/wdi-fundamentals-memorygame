b2a79651504660327154ed4535a98b74361b2a91
Matt Kersner <mattkersner@gmail.com>
2016-11-21 14:38:57 -0500

    created cards using Javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,10 +4,21 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardThree === cardFour) {
+/*if (cardThree === cardFour) {
 	alert("You found a match!");
 } else if (cardOne === cardTwo) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.");
-}
+}*/
+
+var createCards = function () {
+	var board = document.getElementById('game-board');
+
+	for (var i = 0; i < 4; i++) {
+		var card = document.createElement('div');
+		card.className = 'card';
+		board.appendChild(card);
+	}
+};
+createCards();
